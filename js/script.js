6c904695bb6d7599a4462aa667b3ecfd921d7ec5
park <goslwjddl@naver.com>
2023-04-27 17:37:47 +0900

    feat : 버튼 스크롤

--- js/script.js
+++ js/script.js
@@ -39,26 +39,34 @@ $(function () {
   $(".slick-prev").text("prev");
   // information slide
   $(".slide2").slick({
-    arrows:false,
-    dots:true,
-    autoplay:true,
-    infinite:true,
-    slidesToShow:2,
-    slidesToScroll:1,
-    autoplaySpeed:6000,
-    pauseOnHover:true, 
-    pauseOnFocus:true
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplaySpeed: 6000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
   });
-  $(".slide2 #slick-slide-control10").text('서울 마곡지구 업무용지')
-  $(".slide2 #slick-slide-control11").text('서울 마곡지구 대방디엠시티2차')
-  $(".slide2 #slick-slide-control12").text('화성동탄1차 대방디엠시티 더 센텀')
-  $(".slide2 #slick-slide-control13").text('광주 수완 대방노블랜드6차')
+  $(".slide2 #slick-slide-control10").text("서울 마곡지구 업무용지");
+  $(".slide2 #slick-slide-control11").text("서울 마곡지구 대방디엠시티2차");
+  $(".slide2 #slick-slide-control12").text("화성동탄1차 대방디엠시티 더 센텀");
+  $(".slide2 #slick-slide-control13").text("광주 수완 대방노블랜드6차");
 });
 
-// scrolla 
-$(function() {
-	$('.animate').scrolla({
-		mobile: true, //모바일 활성화
-		once: false 
-	});    
+// scrolla
+$(function () {
+  $(".animate").scrolla({
+    mobile: true, //모바일 활성화
+    once: false,
+  });
+});
+
+//scroll down
+$(function () {
+  $(".scroll").on("click", function () {
+    var scrollBtn = $("#scroll").offset().top;
+    $("html,body").animate({ scrollTop: scrollBtn }, 400);
+  });
 });
